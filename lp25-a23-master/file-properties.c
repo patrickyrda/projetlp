3305a0cbe9c336acb7c7b74cfb8314812714c2bc
ivannvasic <151573188+ivannvasic@users.noreply.github.com>
2023-11-24 12:14:30 +0100

    UPDATE FILE PROPERTIES
    Almost every fonctions and a reference to the file configuration.c in the function get_files_stats

--- lp25-a23-master/file-properties.c
+++ lp25-a23-master/file-properties.c
@@ -26,7 +26,31 @@
  *   - entry type (DOSSIER)
  * @return -1 in case of error, 0 else
  */
-int get_file_stats(files_list_entry_t *entry) {
+int get_file_stats(files_list_entry_t *entry, const configuration_t *config) {
+    struct stat statbuf;
+
+    if (stat(entry->path, &statbuf) == -1) {
+        return -1;
+    }
+
+    entry->mode = statbuf.st_mode;
+    entry->mtime = statbuf.st_mtim.tv_nsec;
+
+    if (S_ISREG(statbuf.st_mode)) {
+        entry->size = statbuf.st_size;
+        entry->type = FICHIER;
+
+        // Calcule le MD5 uniquement si l'option est activée dans la configuration
+        if (config->uses_md5) {
+            if (compute_file_md5(entry) == -1) {
+                return -1;
+            }
+        }
+    } else if (S_ISDIR(statbuf.st_mode)) {
+        entry->type = DOSSIER;
+    }
+
+    return 0;
 }
 
 /*!
@@ -36,6 +60,28 @@ int get_file_stats(files_list_entry_t *entry) {
  * Use libcrypto functions from openssl/evp.h
  */
 int compute_file_md5(files_list_entry_t *entry) {
+    FILE *file = fopen(entry->path, "rb");
+    if (!file) {
+        return -1;
+    }
+
+    unsigned char c[MD5_DIGEST_LENGTH];
+    MD5_CTX mdContext;
+    int bytes;
+    unsigned char data[1024];
+
+    MD5_Init(&mdContext);
+    while ((bytes = fread(data, 1, 1024, file)) != 0) {
+        MD5_Update(&mdContext, data, bytes);
+    }
+    MD5_Final(c, &mdContext);
+
+    for(int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+        sprintf(&entry->md5sum[i*2], "%02x", (unsigned int)c[i]);
+    }
+
+    fclose(file);
+    return 0;
 }
 
 /*!
@@ -44,6 +90,8 @@ int compute_file_md5(files_list_entry_t *entry) {
  * @return true if directory exists, false else
  */
 bool directory_exists(char *path_to_dir) {
+    struct stat statbuf;
+    return (stat(path_to_dir, &statbuf) == 0 && S_ISDIR(statbuf.st_mode));
 }
 
 /*!
@@ -53,4 +101,8 @@ bool directory_exists(char *path_to_dir) {
  * Hint: try to open a file in write mode in the target directory.
  */
 bool is_directory_writable(char *path_to_dir) {
+    if (access(path_to_dir, W_OK) == -1) {
+        return false;
+    }
+    return true;
 }
