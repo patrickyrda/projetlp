cb922dc4d239fbc9983d6b8d92146997b79ec5e9
ivannvasic <151573188+ivannvasic@users.noreply.github.com>
2023-12-09 20:01:43 +0100

    Update files-list.c

--- lp25-a23-master/files-list.c
+++ lp25-a23-master/files-list.c
@@ -27,8 +27,38 @@ void clear_files_list(files_list_t *list) {
  *  @param file_path the full path (from the root of the considered tree) of the file
  *  @return 0 if success, -1 else (out of memory)
  */
-files_list_entry_t *add_file_entry(files_list_t *list, char *file_path) {
+files_list_entry_t *add_file_entry(files_list_t *list, char *file_path, configuration_t *config) {
+    for (int i = 0; i < list->count; i++) {
+        if (strcmp(list->entries[i].path, file_path) == 0) {
+            return 0;
+        }
+    }
+    files_list_entry_t *new_entry = (files_list_entry_t *)malloc(sizeof(files_list_entry_t));
+    if (new_entry == NULL) {
+        return -1;
+    }
+    new_entry->path = strdup(file_path);
+    if (new_entry->path == NULL) {
+        free(new_entry);
+        return -1;
+    }
+    if (get_file_stats(new_entry, config) == -1) {
+        free(new_entry->path);
+        free(new_entry);
+        return -1;
+    }
+    int insert_index = 0;
+    while (insert_index < list->count && strcmp(list->entries[insert_index].path, file_path) < 0) {
+        insert_index++;
+    }
+    for (int i = list->count; i > insert_index; i--) {
+        list->entries[i] = list->entries[i - 1];
+    }
+    list->entries[insert_index] = *new_entry;
+    list->count++;
+    return 0;
 }
+
 
 /*!
  * @brief add_entry_to_tail adds an entry directly to the tail of the list
@@ -69,7 +99,20 @@ int add_entry_to_tail(files_list_t *list, files_list_entry_t *entry) {
  *  @return a pointer to the element found, NULL if none were found.
  */
 files_list_entry_t *find_entry_by_name(files_list_t *list, char *file_path, size_t start_of_src, size_t start_of_dest) {
+    files_list_entry_t *current = list->head;
+    while (current != NULL) {
+        int cmp_src = strcmp(current->file_path + start_of_src, file_path + start_of_src);
+        int cmp_dest = strcmp(current->file_path + start_of_dest, file_path + start_of_dest);
+        if (cmp_src == 0 && cmp_dest == 0) {
+            return current; 
+        } else if (cmp_src > 0 || cmp_dest > 0) {
+            break;
+        }
+        current = current->next; 
+    }
+    return NULL;
 }
+
 
 /*!
  * @brief display_files_list displays a files list
